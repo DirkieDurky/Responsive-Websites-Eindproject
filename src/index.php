b5ce523bff415f1caa31b932a6077c20a7705a16
DirkieDurky <dirk@freijters.nl>
2023-10-18 16:07:46 +0200

    Contact layout

--- src/index.php
+++ src/index.php
@@ -3,50 +3,50 @@
 require_once "classes/mechanic.php";
 
 ob_start();
-require "pages/mechanics.php";
+require_once "pages/mechanics.php";
 
 file_put_contents("dist/mechanics/index.html", ob_get_contents());
 ob_end_clean();
 
 ob_start();
-require "pages/contact.php";
+require_once "pages/contact.php";
 
 file_put_contents("dist/contact/index.html", ob_get_contents());
 ob_end_clean();
 
 ob_start();
-require "pages/over-ons.php";
+require_once "pages/over-ons.php";
 
 file_put_contents("dist/over-ons/index.html", ob_get_contents());
 ob_end_clean();
 
 ob_start();
-require "pages/photo-gallery.php";
+require_once "pages/photo-gallery.php";
 
 file_put_contents("dist/photo-gallery/index.html", ob_get_contents());
 ob_end_clean();
 
 ob_start();
-require "pages/story.php";
+require_once "pages/story.php";
 
 file_put_contents("dist/story/index.html", ob_get_contents());
 ob_end_clean();
 
 ob_start();
-require "pages/techniques.php";
+require_once "pages/techniques.php";
 
 file_put_contents("dist/techniques/index.html", ob_get_contents());
 ob_end_clean();
 
 ob_start();
-require "pages/home.php";
+require_once "pages/home.php";
 
 file_put_contents("dist/index.html", ob_get_contents());
 ob_end_clean();
 
 foreach ($mechanics as $mechanic) {
     ob_start();
-    require "pages/mechanic.php";
+    require_once "pages/mechanic.php";
     file_put_contents("dist/mechanics/mechanic/{$mechanic->name}.html", ob_get_contents());
     ob_end_clean();
 }
