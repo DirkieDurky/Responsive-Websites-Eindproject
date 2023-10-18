67f0edc2c3b2d9c76f133f443078a1bb9fc17c3d
DirkieDurky <dirk@freijters.nl>
2023-10-18 15:45:14 +0200

    Transition to php

--- src/index.php
+++ src/index.php
@@ -7,3 +7,39 @@ require "pages/mechanics.php";
 
 file_put_contents("dist/mechanics/index.html", ob_get_contents());
 ob_end_clean();
+
+ob_start();
+require "pages/contact.php";
+
+file_put_contents("dist/contact/index.html", ob_get_contents());
+ob_end_clean();
+
+ob_start();
+require "pages/over-ons.php";
+
+file_put_contents("dist/over-ons/index.html", ob_get_contents());
+ob_end_clean();
+
+ob_start();
+require "pages/photo-gallery.php";
+
+file_put_contents("dist/photo-gallery/index.html", ob_get_contents());
+ob_end_clean();
+
+ob_start();
+require "pages/story.php";
+
+file_put_contents("dist/story/index.html", ob_get_contents());
+ob_end_clean();
+
+ob_start();
+require "pages/techniques.php";
+
+file_put_contents("dist/techniques/index.html", ob_get_contents());
+ob_end_clean();
+
+ob_start();
+require "pages/home.php";
+
+file_put_contents("dist/index.html", ob_get_contents());
+ob_end_clean();
---
+++ src/pages/contact.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Celeste - Contact</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    [Insert header here]<br>
+    <main class="contact">
+        Heeft u een vraag, suggestie, of wilt u om een andere reden contact met ons opnemen? Dat kan via dit formulier!
+        <form class="contact-form">
+            <label>
+                Email<br>
+                <input type="text" placeholder="Email">
+            </label>
+            <label>
+                Naam<br>
+                <input type="text" placeholder="Naam">
+            </label>
+            <label>
+                Bericht<br>
+                <textarea placeholder="Bericht"></textarea>
+            </label>
+        </form>
+    </main>
+</body>
+
+</html>
