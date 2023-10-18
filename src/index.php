2581e0f1103affb17f0252147bdeb5a5769af0a6
NuggetInc <cdl.0404@gmail.com>
2023-10-18 14:59:11 +0200

    new project structure

--- src/index.php
+++ src/index.php
@@ -5,5 +5,5 @@ require_once "classes/mechanic.php";
 ob_start();
 require "pages/mechanics.php";
 
-file_put_contents("output/mechanics/index.html", ob_get_contents());
+file_put_contents("dist/mechanics/index.html", ob_get_contents());
 ob_end_clean();
