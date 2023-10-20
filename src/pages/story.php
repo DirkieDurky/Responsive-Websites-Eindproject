e4f8bfa23f0e68ab8d92b627e7493ba346b23723
DirkieDurky <dirk@freijters.nl>
2023-10-20 06:15:15 +0200

    Add icon

--- src/pages/story.php
+++ src/pages/story.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Celeste - Story</title>
+    <link rel="icon" href="../assets/ico.png">
     <link rel="stylesheet" type="text/css" href="../css/style.css">
     <link rel="stylesheet" type="text/css" href="../css/header.css">
     <link rel="stylesheet" type="text/css" href="../css/pages.css">
